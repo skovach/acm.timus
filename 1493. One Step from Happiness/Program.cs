aa0d11e5297491e02a77ff4a8068508432065604
unknown <skovach@SILVA.softservecom.com>
2015-08-21 11:32:56 +0300

    1493

---
+++ 1493. One Step from Happiness/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1493.One_Step_from_Happiness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticket = Console.ReadLine();
+
+            string firstHalf = ticket.Substring(0, 3);
+            string secondHalf = ticket.Substring(3, 3);
+
+            int fSum = GetSumOfHalf(firstHalf);
+            int sSumInc = GetSumOfHalf((int.Parse(secondHalf) + 1).ToString());
+            int sSumDec = GetSumOfHalf((int.Parse(secondHalf) - 1).ToString());
+
+            string response = fSum == sSumInc || fSum == sSumDec ? "Yes" : "No";
+
+            Console.WriteLine(response);
+        }
+
+        static int GetSumOfHalf(string halfNumber)
+        {
+            return halfNumber.Sum(digit => int.Parse(digit.ToString()));
+        }
+    }
+}
