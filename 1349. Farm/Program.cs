986615b2f1a7bf71207de644a3ce46801eaf5d22
unknown <skovach@SILVA.softservecom.com>
2015-08-25 12:33:22 +0300

    Ferma Theorem

---
+++ 1349. Farm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1349.Farm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (n != 1 && n != 2)
+            {
+                Console.WriteLine("-1");
+                return;
+            }
+
+            string answer1 = "1 2 3";
+            string answer2 = "3 4 5";
+
+            Console.WriteLine(n == 1 ? answer1 : answer2);
+
+        }
+    }
+}
