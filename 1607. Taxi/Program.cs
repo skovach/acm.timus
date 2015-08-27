86cc9822366ada8dcb4699781d2b5ea61413be86
unknown <skovach@SILVA.softservecom.com>
2015-08-27 13:49:05 +0300

    1607 Taxi...

---
+++ 1607. Taxi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1607.Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] prices = Console.ReadLine().Split(' ');
+
+            int petrPrice = int.Parse(prices[0]);
+            int increment = int.Parse(prices[1]);
+            int driverPrice = int.Parse(prices[2]);
+            int reduction = int.Parse(prices[3]);
+
+            while (petrPrice <= driverPrice)
+            {
+                petrPrice += increment;
+
+                if (petrPrice > driverPrice)
+                {
+                    Console.WriteLine(driverPrice);
+                    return;
+                }
+
+                driverPrice -= reduction;
+            }
+
+            Console.WriteLine(petrPrice);
+        }
+    }
+}
