33fd2dc2ed54bae09ada85df7364721eb61afd5b
Sergii Kovach <skovach@SILVA.softservecom.com>
2015-09-06 13:30:16 +0300

    1496 & 1925

---
+++ 1496. Spammer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1496.Spammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> teams = new HashSet<string>();
+            HashSet<string> spammers = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string team = Console.ReadLine();
+
+                if (teams.Contains(team))
+                {
+                    spammers.Add(team);
+                }
+                else
+                {
+                    teams.Add(team);
+                }
+            }
+
+            foreach (var spammer in spammers)
+            {
+                Console.WriteLine(spammer);
+            }
+        }
+    }
+}
---
+++ 1925. British Scientists Save the World/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1925.British_Scientists_Save_the_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] get = Console.ReadLine().Split(' ');
+            int n = int.Parse(get[0]);
+            int k = int.Parse(get[1]);
+            List<int> years = new List<int>();
+            List<int> inNumbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                years.Add(int.Parse(line[0]));
+                inNumbers.Add(int.Parse(line[1]));
+            }
+
+
+            int x = 0;
+
+            x = (years.Sum() + k - (n + 1)*2) - inNumbers.Sum();
+
+            Console.WriteLine(x > 0? x.ToString() : "Big Bang!");
+        }
+    }
+}
