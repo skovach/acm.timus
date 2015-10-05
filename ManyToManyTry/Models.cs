1e095c18224a546b25eef62b1da73b28a0437ea6
Sergii Kovach <skovach@SILVA.softservecom.com>
2015-10-05 12:42:04 +0300

    Many To Many

---
+++ ManyToManyTry/Models.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyToManyTry
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Course> CoursesAttending { get; set; }
+
+        public Person()
+        {
+            CoursesAttending = new HashSet<Course>();
+        }
+    }
+
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<Person> Students { get; set; }
+
+        public Course()
+        {
+            Students = new HashSet<Person>();
+        }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        public SchoolContext()
+          : base("SchoolDb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().
+              HasMany(c => c.Students).
+              WithMany(p => p.CoursesAttending).
+              Map(
+               m =>
+               {
+                   m.MapLeftKey("CourseId");
+                   m.MapRightKey("PersonId");
+                   m.ToTable("PersonCourses");
+               });
+        }
+    }
+}
+
---
+++ ManyToManyTry/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyToManyTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolContext context = new SchoolContext();
+
+            var s = context.Courses.Select(x => x.Students); 
+        }
+    }
+}
