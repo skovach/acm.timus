6ce17831dcf022df4f95745af8da83c93fe66fd3
SOFTSERVE\skovach <skovach@softserveinc.com>
2017-01-26 15:16:16 +0200

    2035. Another Dress Rehearsal

---
+++ 2035. Another Dress Rehearsal/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _2035.Another_Dress_Rehearsal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            UInt64 x = UInt64.Parse(input[0]);
+            UInt64 y = UInt64.Parse(input[1]);
+            UInt64 c = UInt64.Parse(input[2]);
+
+            if (ImpossibleConditions(x, y, c))
+            {
+                Console.WriteLine("Impossible");
+                return;
+            }
+            UInt64 a, b;
+            
+            if (x >= c)
+            {
+                Console.WriteLine("{0} {1}", c, 0);
+                return;
+            }
+            if (y >= c)
+            {
+                Console.WriteLine("{0} {1}", 0, c);
+                return;
+            }
+
+            bool reverseResult = x > y;
+            if (reverseResult)
+            {
+                var temp = y;
+                y = x;
+                x = temp;
+            }
+
+            while (x/2 + y >= c)
+            {
+                if (x/2 + y == c)
+                {
+                    a = x/2;
+                    b = y;
+                    if (reverseResult)
+                    {
+                        var temp = b;
+                        b = a;
+                        a = temp;
+                    }
+                    Console.WriteLine("{0} {1}", a, c - a);
+                }
+
+                x /= 2;
+            }
+
+            for (a = x; a < x*2; a++)
+            {
+                b = c - a;
+                if (b >= 0 && b <= y)
+                {
+                    if (reverseResult)
+                    {
+                        var temp = b;
+                        b = a;
+                        a = temp;
+                    }
+                    Console.WriteLine("{0} {1}", a, b);
+                    return;
+                }
+            }
+
+            
+        }
+
+    
+
+        private static bool ImpossibleConditions(ulong x, ulong y, ulong c)
+        {
+            return x + y < c;
+        }
+    }
+}
---
+++ RefVSValie/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefVSValie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int min = 0;
+            int max = 100;
+            var list = Enumerable.Repeat(0, 10).Select(i => rand.Next(min, max)).ToList();
+            MethodParams(list);
+
+            Console.WriteLine();
+        }
+
+        public static void MethodParams(List<int> list)
+        {
+            list[0] = 42;
+
+            list = new List<int>();
+            
+
+        }
+    }
+}
