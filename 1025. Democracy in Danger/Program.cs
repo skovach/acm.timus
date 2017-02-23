95145231c3f4b0edea4a856d9e29d3ab6fbe2f82
sergii.kovach-home <devskovach@gmail.com>
2017-02-23 16:23:19 +0200

    1224 nor ready 1025 done

---
+++ 1025. Democracy in Danger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1025.Democracy_in_Danger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            List<string> votersLine = Console.ReadLine().Split(' ').ToList();
+            int result = 0;
+
+            // solution 1 ok
+            var voters = new List<int>();
+            votersLine.ForEach(x => { voters.Add(int.Parse(x)); });
+            voters.Sort();
+            for (int i = 0; i < (k / 2) + 1; i++)
+            {
+                result += (voters[i] / 2) + 1;
+            }
+
+            // not working with set of equal items. example: 5 5 5 
+            //votersLine.Sort();
+            //int groupAmount = (k / 2) + 1;
+            //votersLine.RemoveRange(groupAmount, votersLine.Count - groupAmount);
+
+            //var prev = votersLine[0];
+
+            //List<int> voters = new List<int>();
+            //votersLine.ForEach(v => voters.Add(int.Parse(v)));
+
+            //result = (votersLine.Sum(x => int.Parse(x)) / 2) + 1;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 1224. Spiral/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1224.Spiral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> field = Console.ReadLine().Split(' ').ToList();
+            int m = int.Parse(field[0]);
+            int n = int.Parse(field[1]);
+
+            int turns = 0;
+
+            int minSide = m > n ? n : m;
+            int isOdd = minSide%2 == 0 ? 0 : 1;
+
+            int spirals = ((minSide-isOdd) / 2) * 4;
+
+            Console.WriteLine(spirals);
+        }
+    }
+}
